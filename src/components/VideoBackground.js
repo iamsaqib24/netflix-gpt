f010b1e40a884a6e48b628c5e3ebb4b17a18aa2a
Saqib <iamsaqib24@gmail.com>
2023-09-01 16:35:37 +0530

    built Main component and created another custom hook for fetching movie trailer

---
+++ src/components/VideoBackground.js
@@ -0,0 +1,21 @@
+import React from "react";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+import { useSelector } from "react-redux";
+
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+
+  useMovieTrailer(movieId);
+
+  return (
+    <div>
+      <iframe
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+};
+
+export default VideoBackground;
