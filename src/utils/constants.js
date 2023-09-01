e683bef04aaaf18d83b3791534fe20e6a3879c37
Saqib <iamsaqib24@gmail.com>
2023-09-01 14:55:49 +0530

    made api call and stored data into moviesStore

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://occ-0-6245-2186.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABWtYstzFdHB5VDwg1wo0AtmxHp1wtiHQt6kfVzqrLag-FyrhJSX1Suc-w_56yVQPsLfndT57n9KLPVZ-PMVtsUAKpA3TZ6M.png?r=72f";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTY0MDc0OGIxNWE2YmVkMmI2MGZmMDg3ZTNhMDFlYyIsInN1YiI6IjY0ZjE3MDlmZWJiOTlkMDBlM2U3ZjhlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AeG5wC0duaha9ftQhTQyS0jSEqSmS7JGNltknJ7s8WE",
+  },
+};
